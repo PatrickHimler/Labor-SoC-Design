697c00244bc6f0cb435080c053bd893a294e79d8
Lukas Vogl <l.vogl1996@gmx.at>
2021-02-22 11:38:52 +0100

    Addes JA1 as output for UART

--- zedboard_aes-master/build.tcl
+++ zedboard_aes-master/build.tcl
@@ -283,6 +283,7 @@ proc cr_bd_system { parentCell } {
   set SDIN [ create_bd_port -dir O SDIN ]
   set VBAT [ create_bd_port -dir O VBAT ]
   set VDD [ create_bd_port -dir O VDD ]
+  set JA1 [ create_bd_port -dir O JA1 ]
 
   # Create instance: AES_PROCESS_0, and set properties
   set AES_PROCESS_0 [ create_bd_cell -type ip -vlnv sfu.ca:user:AES_PROCESS:1.0 AES_PROCESS_0 ]
@@ -771,7 +772,8 @@ proc cr_bd_system { parentCell } {
   connect_bd_net -net processing_system7_0_FCLK_RESET0_N [get_bd_pins processing_system7_0/FCLK_RESET0_N] [get_bd_pins rst_ps7_0_100M/ext_reset_in]
   connect_bd_net -net rst_ps7_0_100M_interconnect_aresetn [get_bd_pins ps7_0_axi_periph/ARESETN] [get_bd_pins rst_ps7_0_100M/interconnect_aresetn]
   connect_bd_net -net rst_ps7_0_100M_peripheral_aresetn [get_bd_pins AES_PROCESS_0/AXIS_ARESETN] [get_bd_pins AES_PROCESS_0/s00_axi_aresetn] [get_bd_pins pwr_mon_0/nrst] [get_bd_pins AMA142_OLED_AXI/s00_axi_aresetn] [get_bd_pins BTN_GPIO_AXI/s_axi_aresetn] [get_bd_pins SW_LED_GPIO_AXI/s_axi_aresetn] [get_bd_pins axi_dma_0/axi_resetn] [get_bd_pins axi_smc/aresetn] [get_bd_pins ps7_0_axi_periph/M00_ARESETN] [get_bd_pins ps7_0_axi_periph/M01_ARESETN] [get_bd_pins ps7_0_axi_periph/M02_ARESETN] [get_bd_pins ps7_0_axi_periph/M03_ARESETN] [get_bd_pins ps7_0_axi_periph/M04_ARESETN] [get_bd_pins ps7_0_axi_periph/S00_ARESETN] [get_bd_pins rst_ps7_0_100M/peripheral_aresetn]
-
+  connect_bd_net [get_bd_ports JA1] [get_bd_pins pwr_mon_0/serial_tx]
+  
   # Create address segments
   create_bd_addr_seg -range 0x20000000 -offset 0x00000000 [get_bd_addr_spaces axi_dma_0/Data_MM2S] [get_bd_addr_segs processing_system7_0/S_AXI_HP0/HP0_DDR_LOWOCM] SEG_processing_system7_0_HP0_DDR_LOWOCM
   create_bd_addr_seg -range 0x20000000 -offset 0x00000000 [get_bd_addr_spaces axi_dma_0/Data_S2MM] [get_bd_addr_segs processing_system7_0/S_AXI_HP0/HP0_DDR_LOWOCM] SEG_processing_system7_0_HP0_DDR_LOWOCM
--- zedboard_aes-master/src/system.xdc
+++ zedboard_aes-master/src/system.xdc
@@ -4,6 +4,7 @@ set_property PACKAGE_PIN AB12 [get_ports SCLK]
 set_property PACKAGE_PIN AA12 [get_ports SDIN]
 set_property PACKAGE_PIN U11 [get_ports VBAT]
 set_property PACKAGE_PIN U12 [get_ports VDD]
+set_property PACKAGE_PIN Y11  [get_ports JA1];  # "JA1"
 
 set_property IOSTANDARD LVCMOS33 [get_ports DC]
 set_property IOSTANDARD LVCMOS33 [get_ports RES]
